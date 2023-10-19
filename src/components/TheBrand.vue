4c6fc64c3c73ea7a8c3f81b4897e80f9a2088dc7
akramjonnuriddinov <akramjonmohirdev@gmail.com>
2023-10-19 17:46:55 +0500

    Added responsiveness to Brand

--- src/components/TheBrand.vue
+++ src/components/TheBrand.vue
@@ -4,15 +4,18 @@
       class="container relative flex px-5 pb-16 mx-auto transition-all duration-300 max-w-7xl"
     >
       <swiper
-        :slidesPerView="6"
+        :slidesPerView="count"
         :spaceBetween="0"
         :pagination="{
           clickable: true,
         }"
         class="mySwiper"
       >
-        <swiper-slide v-for="brand in brands">
-          <div class="w-full">
+        <swiper-slide
+          class="swiper-slide w-1/6 max-[990px]:w-1/3"
+          v-for="brand in brands"
+        >
+          <div class="flex justify-center w-full">
             <img
               class="block transition-all cursor-pointer brand-item-img opacity-60 hover:opacity-100 grayscale hover:grayscale-0"
               :src="getImageUrl(brand)"
@@ -26,8 +29,24 @@
 </template>
 
 <script setup lang="ts">
-import { ref } from "vue"
+import { ref, onMounted } from "vue"
 import { Swiper, SwiperSlide } from "swiper/vue"
+
+const count = ref(3)
+onMounted(() => {
+  window.addEventListener("resize", () => {
+    if (window.screen.availWidth <= 990 && window.screen.availWidth > 700) {
+      count.value = 3
+    } else if (
+      window.screen.availWidth <= 700 &&
+      window.screen.availWidth > 400
+    ) {
+      count.value = 2
+    } else if (window.screen.availWidth <= 400) {
+      count.value = 1
+    }
+  })
+})
 
 const brands = ref([
   "brand_img01.png",
