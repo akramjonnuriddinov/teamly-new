4e7fe019ad35ab002030fc692c01ff3519cee506
rajabovdiyorbek <m.mirzajanov@seclgroup.com>
2023-12-05 19:27:20 +0500

    minor fix

--- src/components/profile/Details.vue
+++ src/components/profile/Details.vue
@@ -14,7 +14,7 @@
           name="name"
           @input="updateFirebaseName"
           v-model="user.name"
-          placeholder="Diyorbek Rajabov"
+          placeholder="name"
           class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5"
         />
       </div>
