6147d582e4177c13f56a933f228d98afe9c04ed2
rajabovdiyorbek <m.mirzajanov@seclgroup.com>
2023-12-05 16:19:20 +0500

    disable navs

--- src/pages/profile/Sidebar.vue
+++ src/pages/profile/Sidebar.vue
@@ -1,9 +1,9 @@
 <template>
-  <aside class="relative top-10 left-0 z-40 w-64 h-screen transition-transform -translate-x-full sm:translate-x-0">
+  <aside class="relative fixed top-10 left-0 z-40 w-64 h-screen transition-transform -translate-x-full sm:translate-x-0">
     <div class="h-full px-3 py-4 overflow-y-auto bg-gray-50">
       <h1 class="text-2xl font-medium">Your Profile</h1>
       <ul class="mt-[57px]" >
-        <li v-for="nav in navigations"  class="mt-[30px] w-full font-semibold text-base cursor-pointer hover:text-tg-primary-color" :key="nav.name" @click="selectNavItem(nav.name)"><span :class="{ 'bg-tg-primary-color text-white rounded-[5px]': selectedNavItem === nav.name }" class="py-3 px-5">{{ nav.name }}</span></li>
+        <li v-for="nav in navigations" onmousedown='return false;' onselectstart='return false;' :class="{'text-gray-400 cursor-not-allowed ' : nav.disabled , 'hover:text-tg-primary-color' : !nav.disabled}" class="mt-[30px] w-full font-semibold text-base cursor-pointer" :key="nav.name" @click="selectNavItem(nav.name, nav.disabled)"><span :class="{ 'bg-tg-primary-color text-white rounded-[5px]': selectedNavItem === nav.name }" class="py-3 px-5">{{ nav.name }}</span></li>
       </ul>
     </div>
   </aside>
@@ -17,24 +17,32 @@ const selectedNavItem = ref('Profile details')
 const navigations = ref([
   {
     name: 'Profile details',
+    disabled: false
   },
   {
     name: 'Applied vacancies',
+    disabled: false
   },
   {
     name: 'Applied internships',
+    disabled: true
   },
   {
     name: 'My Resumes',
+    disabled: true
+
   },
   {
     name: 'Messages from Teamly',
+    disabled: true
   },
 ])
 
 
-const selectNavItem = (itemName:string) => {
-  selectedNavItem.value = itemName
-  emit('selectItem', itemName)
+const selectNavItem = (itemName:string, disabled: boolean) => {
+  if(!disabled) {
+    selectedNavItem.value = itemName
+    emit('selectItem', itemName)
+  }
 }
 </script>
