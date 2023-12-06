1787bb4a4f6f45f5a0e80f3a7ffbf118fee8e87f
Rajabov Diyorbek <rajabov.diyorbek.it@gmail.com>
2023-12-06 11:55:03 +0500

    add resume info to profile page

--- src/components/profile/Details.vue
+++ src/components/profile/Details.vue
@@ -1,5 +1,4 @@
 <template>
-  <!-- Your HTML template -->
   <div class="relative pb-[135px] border-b border-gray-300">
     <div class="mb-7">
       <h1 class="text-[20px] font-medium">Personal information</h1>
@@ -30,6 +29,41 @@
         />
       </div>
     </div>
+    <div class="flex justify-between mt-5">
+      <div class="flex flex-col w-[400px]">
+        <label for="name" class="block mb-2 text-sm font-medium text-gray-900">Your telegram username</label>
+        <input
+          type="text"
+          required
+          @input="updateFirebaseName"
+          placeholder="t.me/"
+          class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5"
+        />
+      </div>
+      <div class="flex flex-col w-[400px]">
+        <label for="email" class="block mb-2 text-sm font-medium text-gray-900">Your phone number</label>
+        <input
+          type="text"
+          required
+          @input="updateFirebaseEmail"
+          placeholder="Phone number"
+          class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5"
+        />
+      </div>
+    </div>
+    <div class="flex flex-col w-[400px] mt-5">
+      <div class="mb-6 w-full max-[800px]:w-full relative">
+        <label for="file-input" class="block mb-2 text-sm font-medium text-gray-900">Upload your CV</label>
+        <input
+          class="block w-full p-3 text-sm border border-gray-300 text-gray-900 rounded-md shadow-sm cursor-pointer file:hidden"
+          @change="handleFileChange"
+          accept=".docx,.pdf,.txt"
+          type="file"
+          name="file-input"
+          id="file-input"
+        />
+      </div>
+    </div>
     <button
       @click="updateProfileInformation"
       class="w-[250px] transition-all duration-300 bg-tg-primary-color text-tg-white hover:bg-tg-secondary-color absolute right-0 bg-[#7e54f8] text-white mt-[35px] focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-[30px] py-[8px] text-center"
@@ -55,6 +89,11 @@ const updatedUser = ref({
   name: user.name,
   email: user.email,
 })
+
+const selectedFile = ref<any>(null)
+const handleFileChange = (event: any) => {
+  selectedFile.value = event.target.files[0]
+}
 
 const updateFirebaseName = (event: any) => {
   const newValue = event.target.value
