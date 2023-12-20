2329092926df466ae151f26c804fb52c66da362a
rajabovdiyorbek <rajabov.diyorbek.it@gmail.com>
2023-12-20 15:57:39 +0500

    fix

--- src/components/job/JobLists.vue
+++ src/components/job/JobLists.vue
@@ -1,7 +1,30 @@
 <template>
   <section id="vacancies" class="pt-[115px] pb-[45px] relative z-10 mb-[100px]">
     <div class="container relative w-full px-5 mx-auto max-w-7xl">
-      <ul class="flex flex-wrap justify-start">
+      <ul v-if="listLoading" class="flex flex-wrap justify-start">
+        <li
+          class="w-1/3 px-2.5 py-2.5 service-item max-[1050px]:w-1/2 max-[710px]:w-full"
+          v-for="vacancy in 4"
+          :key="vacancy"
+        >
+          <div class="shadow-job-inner bg-white flex flex-col h-full rounded-[32px] px-[35px] py-[50px]">
+            <span class="mb-5"><Skeleton width="100%" height="24px" :theme="ESkeletonTheme.LIGHT" /></span>
+            <div
+              class="text-3xl font-bold text-[#1C1C37] leading-[1.2em] mb-7 transition-all duration-300 hover:text-tg-secondary-color"
+            >
+              <Skeleton width="100%" height="72px" :theme="ESkeletonTheme.LIGHT" />
+            </div>
+            <div class="text-tg-primary-color tracking-[-0.3px] font-bold flex items-center gap-3 mb-5">
+              <Skeleton width="70px" height="24px" :theme="ESkeletonTheme.LIGHT" />
+              <Skeleton width="8px" height="8px" :theme="ESkeletonTheme.LIGHT" />
+              <Skeleton width="116px" height="24px" :theme="ESkeletonTheme.LIGHT" />
+            </div>
+            <p class="mb-12"><Skeleton width="100%" height="24px" :theme="ESkeletonTheme.LIGHT" /></p>
+            <Skeleton width="100%" height="60px" :theme="ESkeletonTheme.LIGHT" />
+          </div>
+        </li>
+      </ul>
+      <ul v-else class="flex flex-wrap justify-start">
         <li
           class="w-1/3 px-2.5 py-2.5 service-item max-[1050px]:w-1/2 max-[710px]:w-full"
           v-for="vacancy in vacancies"
@@ -48,7 +71,7 @@
 
 <script setup lang="ts">
 import InlineSvg from '@/components/reusables/InlineSvg.vue'
-import { onMounted, ref, watch } from 'vue'
+import { ref, watch } from 'vue'
 import BaseButton from '@/components/reusables/BaseButton.vue'
 import { useAuthStore } from '@/store/auth'
 import { ESize } from '@/types'
@@ -57,7 +80,7 @@ import { fetchData } from '@/composables/fetchData'
 import { collection, where, query, getDocs } from 'firebase/firestore'
 import { useFirestore } from 'vuefire'
 import { vacancyApply } from '@/composables/vacancyApply'
-import { toggleLoader } from '@/composables/loader'
+import Skeleton, { ESkeletonTheme } from '@/components/skeleton/Skeleton.vue'
 
 const props = defineProps(['vacancyId'])
 const db = useFirestore()
@@ -66,11 +89,12 @@ const vacancies = ref()
 const store = useAuthStore()
 const user = ref({ ...store.user })
 const isLoading = ref(null)
+const listLoading = ref(true)
 
 const router = useRouter()
 const handleApply = async (id: any) => {
   if (!store.user) {
-    router.push('/sign-in')
+    router.push('/login')
     return
   }
 
@@ -98,7 +122,6 @@ const fetchDataAndApply = async () => {
     currentApply(props.vacancyId)
   } else {
     try {
-      toggleLoader(true)
       const result = await fetchData('vacancies')
       vacancies.value = result
 
@@ -116,21 +139,16 @@ const fetchDataAndApply = async () => {
     } catch (error) {
       console.error(error)
     } finally {
-      toggleLoader()
+      listLoading.value = false
     }
   }
 }
 
-onMounted(() => {
-  fetchDataAndApply()
-})
 watch(
   () => store.user,
-  (newValue) => {
+  async (newValue) => {
     user.value = { ...newValue }
-    if (newValue && newValue.id) {
-      fetchDataAndApply()
-    }
+    await fetchDataAndApply()
   },
   {
     immediate: true,
