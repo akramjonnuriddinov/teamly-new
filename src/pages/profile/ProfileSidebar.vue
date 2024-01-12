f7556ca58192da456f5a1bed9cdb77de97f3fb8b
akramjonnuriddinov <akramjonmohirdev@gmail.com>
2024-01-12 10:45:12 +0500

    Changes Profile responsive from max-w-[700px] to max-w-[990px]

--- src/pages/profile/ProfileSidebar.vue
+++ src/pages/profile/ProfileSidebar.vue
@@ -1,21 +1,21 @@
 <template>
   <aside
-    class="max-[700px]: relative left-0 z-[400] w-64 pt-10 max-[700px]:mt-[100px] max-[700px]:flex max-[700px]:w-full max-[700px]:justify-center max-[700px]:pt-0 sm:translate-x-0"
+    class="max-[990px]: relative left-0 z-[400] w-64 pt-10 max-[990px]:mt-[100px] max-[990px]:flex max-[990px]:w-full max-[990px]:justify-center max-[990px]:pt-0 sm:translate-x-0"
   >
     <div
-      class="max-[700px]:shadow- h-full overflow-y-auto px-3 py-4 max-[700px]:w-full max-[700px]:rounded-2xl max-[700px]:border max-[700px]:p-4"
+      class="max-[990px]:shadow- h-full overflow-y-auto px-3 py-4 max-[990px]:w-full max-[990px]:rounded-2xl max-[990px]:border max-[990px]:p-4"
     >
-      <h1 class="text-2xl font-medium max-[700px]:hidden">Your Profile</h1>
-      <ul class="mt-[57px] flex flex-col max-[700px]:mt-0 max-[700px]:flex-row max-[700px]:justify-between">
+      <h1 class="text-2xl font-medium max-[990px]:hidden">Your Profile</h1>
+      <ul class="mt-[57px] flex flex-col max-[990px]:mt-0 max-[990px]:flex-row max-[990px]:justify-between">
         <li
           v-for="nav in navigations"
           onmousedown="return false;"
           onselectstart="return false;"
           :class="{
-            'cursor-not-allowed text-gray-400 max-[700px]:hidden': nav.disabled,
+            'cursor-not-allowed text-gray-400 max-[990px]:hidden': nav.disabled,
             'cursor-pointer hover:text-tg-primary-color': !nav.disabled,
           }"
-          class="relative mb-3 w-full rounded-md text-base font-semibold max-[700px]:mb-0 max-[700px]:w-[40%] max-[540px]:w-[50%]"
+          class="relative mb-3 w-full rounded-md text-base font-semibold max-[990px]:mb-0 max-[990px]:w-[40%] max-[540px]:w-[50%]"
           :key="nav.name"
           @click="selectNavItem(nav.name, nav.disabled)"
         >
--- src/pages/profile/ProfileView.vue
+++ src/pages/profile/ProfileView.vue
@@ -1,11 +1,11 @@
 <template>
   <section class="mt-[86px] bg-[#F9F9FA]">
     <div
-      class="container mx-auto flex max-w-7xl px-5 max-xl:max-w-[990px] max-[990px]:max-w-3xl max-[800px]:max-w-2xl max-[700px]:flex-col max-[680px]:max-w-xl"
+      class="container mx-auto flex max-w-7xl px-5 max-xl:max-w-[990px] max-[990px]:max-w-3xl max-[990px]:flex-col max-[800px]:max-w-2xl max-[680px]:max-w-xl"
     >
       <profile-sidebar @selectItem="updateSelectedComponent" />
       <div
-        class="main-content relative left-0 top-10 z-40 mt-[85px] min-h-screen w-64 flex-grow py-4 ps-[111px] transition-transform max-[700px]:mt-0 max-[700px]:w-full max-[700px]:ps-4 max-[700px]:pt-0 sm:translate-x-0"
+        class="main-content relative left-0 top-10 z-40 mt-[85px] min-h-screen w-64 flex-grow py-4 ps-[111px] transition-transform max-[990px]:mt-0 max-[990px]:w-full max-[990px]:ps-4 max-[990px]:pt-0 sm:translate-x-0"
       >
         <component :is="selectedComponent"></component>
       </div>
