5c36e3efa7d0f261fa64cd09034ff6d05e9eeb53
rustamidastan <rustamidastan0414@gmail.com>
2024-01-22 11:46:52 +0500

    disable router-link apart from vacancies

--- src/pages/admin/AdminView.vue
+++ src/pages/admin/AdminView.vue
@@ -2,12 +2,12 @@
   <div class="flex">
     <admin-sidebar @change="changeSidebarSelected" :active="sidebarSelected" />
     <router-view v-if="route.path == '/admin/resume'" />
-    <control-panel v-else :title="sidebarSelected" />
+    <control-panel v-else :title="sidebarSelected" :hasInnerPage="hasInnerPage" />
   </div>
 </template>
 
 <script setup lang="ts">
-import { ref } from 'vue'
+import { computed, ref } from 'vue'
 import AdminSidebar from '@/pages/admin/AdminSidebar.vue'
 import ControlPanel from '@/pages/admin/ControlPanel.vue'
 import { useRoute } from 'vue-router'
@@ -15,6 +15,7 @@ import { useRoute } from 'vue-router'
 const route = useRoute()
 
 const sidebarSelected = ref('vacancies')
+const hasInnerPage = computed(() => sidebarSelected.value === 'vacancies')
 
 const changeSidebarSelected = (name: string) => {
   if (name === 'appliers') return
