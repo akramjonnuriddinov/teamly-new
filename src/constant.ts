a0ac762f35fb1eef7d0eb94f40a6de0a01256d0c
akramjonnuriddinov <akramjonmohirdev@gmail.com>
2024-02-17 16:54:07 +0500

    Fix useSeo function

--- src/constant.ts
+++ src/constant.ts
@@ -1,7 +1,6 @@
 export const HOME_VIEW = {
   page: 'home',
   title: 'Teamly UZ - Shaping Digital Dreams!',
-  url: "teamly.uz",
   description:
     'Custom software development company. We create world-class e-Commerce platforms, custom web portals, and enterprise-grade web apps',
   image: "logos/logo.png",
@@ -9,45 +8,40 @@ export const HOME_VIEW = {
 
 export const ABOUT_VIEW = {
   page: 'about',
-  title: 'Teamly UZ - Shaping Digital Dreams!',
-  url: "teamly.uz",
+  title: 'Web development - solutions for various industries | Teamly UZ',
   description:
-    'Custom software development company. We create world-class e-Commerce platforms, custom web portals, and enterprise-grade web apps',
+    'Web development for eCommerce, automotive, financial, travel industries. We also provide high-load web app development services.',
   image: "logos/logo.png",
 }
 
 export const SERVICE_VIEW = {
   page: 'service',
-  title: 'Teamly UZ - Shaping Digital Dreams!',
-  url: "teamly.uz",
+  title: 'Software Solutions | Teamly UZ',
   description:
-    'Custom software development company. We create world-class e-Commerce platforms, custom web portals, and enterprise-grade web apps',
+    'Software development services for businesses — from an MVP for startups to high-load systems and enterprise-level applications.',
   image: "logos/logo.png",
 }
 
 export const PORTFOLIO_VIEW = {
-  page: 'portfolio',
-  title: 'Teamly UZ - Shaping Digital Dreams!',
-  url: "teamly.uz",
+  page: 'project',
+  title: 'Portfolio: See Our Web Development Case Studies',
   description:
-    'Custom software development company. We create world-class e-Commerce platforms, custom web portals, and enterprise-grade web apps',
+    'Website development, custom web programming and web design.',
   image: "logos/logo.png",
 }
 
 export const VACANCY_VIEW = {
   page: 'vacancy',
-  title: 'Teamly UZ - Shaping Digital Dreams!',
-  url: "teamly.uz",
+  title: 'Careers | Teamly Uz!',
   description:
-    'Custom software development company. We create world-class e-Commerce platforms, custom web portals, and enterprise-grade web apps',
+    'Discover job opportunities in Teamly UZ - join our software developers team. Learn about growth opportunities for junior software developer and junior software engineer.',
   image: "logos/logo.png",
 }
 
 export const CONTACT_VIEW = {
   page: 'contact',
-  title: 'Teamly UZ - Shaping Digital Dreams!',
-  url: "teamly.uz",
+  title: 'Contact information | Teamly Uz',
   description:
-    'Custom software development company. We create world-class e-Commerce platforms, custom web portals, and enterprise-grade web apps',
+    'Teamly Uz is a custom software and web application development company. Contact us for a consultation about our software development services in the Uzbekistan | Fergana.',
   image: "logos/logo.png",
 }
